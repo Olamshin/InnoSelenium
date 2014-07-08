d2fff2e6c5da0140ef81ecdce361a4a796fcbc57
okolawole <okolawole@accruent.com>
2014-07-08 11:01:49 -0500

    changed to SiterraComponent

--- UnitTestProject1/UnitTestProject1/Pages/UnitHomePage.cs
+++ UnitTestProject1/UnitTestProject1/Pages/UnitHomePage.cs
@@ -10,7 +10,7 @@ using OpenQA.Selenium.Support.UI;
 
 namespace UnitTestProject1.Pages
 {
-    public class UnitHomePage : UiComponent
+    public class UnitHomePage : SiterraComponent
     {
 
         public void PleaseWait()
