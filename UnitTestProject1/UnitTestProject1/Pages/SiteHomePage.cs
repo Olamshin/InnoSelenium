bd6eb0e298cb57b941831acda53e56ad3947cf27
okolawole <okolawole@accruent.com>
2014-07-08 09:50:44 -0500

    SIteHomePage

---
+++ UnitTestProject1/UnitTestProject1/Pages/SiteHomePage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestStack.Seleno.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using TestStack.Seleno.PageObjects.Actions;
+using OpenQA.Selenium.Support.UI;
+
+namespace UnitTestProject1.Pages
+{
+    public class SiteHomePage : SiterraComponent
+    {
+        public override void PleaseWait()
+        {
+            Host.Wait.Until<Boolean>((d) =>
+            {
+                return !d.PageSource.Contains("indicator_gray.gif");
+                //return !d.FindElement(By.Id("NavContainer")).ToString().Contains("indicator_gray.gif");
+            });
+        }
+    }
+}
