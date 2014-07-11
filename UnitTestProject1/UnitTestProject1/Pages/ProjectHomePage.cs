b04b13b6873cd24049b6cde0a01c7cfc55e12211
okolawole <okolawole@accruent.com>
2014-07-11 18:04:36 -0500

    no message

---
+++ UnitTestProject1/UnitTestProject1/Pages/ProjectHomePage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestStack.Seleno.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using TestStack.Seleno.PageObjects.Actions;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.ObjectModel;
+
+namespace UnitTestProject1.Pages
+{
+    public class ProjectHomePage : SiterraComponent
+    {
+        public override void PleaseWait()
+        {
+            var executor = Host.Instance.Application.Browser as IJavaScriptExecutor;
+            Host.Wait.Until<Boolean>((Browser) =>
+            {
+                return executor.ExecuteScript("return document.readyState").Equals("complete");
+            });
+
+            SwitchIn();
+            Host.Wait.Until<IWebElement>((d) =>
+            {
+                return d.FindElement(By.Id("DataDivContainerview"));
+            });
+            SwitchOut();
+        }
+
+        private void SwitchIn()
+        {
+            Host.Instance.Application.Browser.SwitchTo().Frame("MainFrame");
+        }
+
+        private void SwitchOut()
+        {
+            Host.Instance.Application.Browser.SwitchTo().DefaultContent();
+        }
+    }
+}
